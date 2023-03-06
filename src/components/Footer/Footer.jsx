df5320012d9ec6a0db7f9c9d9da9299da4a8f3f0
ARA <M1RAK>
2023-03-06 15:39:17 +0100

    completed!

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer(){
     return (
         <footer>
-        <div class="attribution">
+        <div className="attribution">
           Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
           Coded by <a href="https://www.frontendmentor.io/profile/M1RAK">M1RAK</a>.
         </div>
